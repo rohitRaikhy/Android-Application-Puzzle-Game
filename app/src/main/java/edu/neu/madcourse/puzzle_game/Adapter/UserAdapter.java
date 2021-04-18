a44f82dfe2691d760390c7f9434150a8412cec85
Rohit Raikhy <raikhy101@gmail.com>
2021-04-18 17:34:28 +0100

    added to the ui for messenger

---
+++ app/src/main/java/edu/neu/madcourse/puzzle_game/Adapter/UserAdapter.java
@@ -0,0 +1,11 @@
+package edu.neu.madcourse.puzzle_game.Adapter;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+//public class UserAdapter extends RecyclerView.Adapter {
+//
+//    private Context mContext;
+//    private List<User> mUsers;
+//}
